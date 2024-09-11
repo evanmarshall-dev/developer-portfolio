cfe6c6a7fd5c419adaa1b6b1214a642ee1016a09
evanmarshall-dev <me@evanmarshall.dev>
2024-09-11 00:58:16 -0300

    DEVELOPER PORTFOLIO: Added a makeshift dynamic mobile menu. It works, but it needs to be styled better.

--- app/(site)/ui/navbar/(client)/Navbar.tsx
+++ app/(site)/ui/navbar/(client)/Navbar.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface Page {
   _id: string;
@@ -17,26 +18,52 @@ interface NavbarProps {
 export default function Navbar({ pages }: NavbarProps) {
   // const pages = await getPages();
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  // function getMenuClasses() {
+  //   let menuClasses = [];
+
+  //   if (isOpen) {
+  //     menuClasses = [
+  //       "flex",
+  //       "flex-col",
+  //       "absolute",
+  //       "top-[200px]",
+  //       "bg-gray-800",
+  //       "w-full",
+  //       "p-4",
+  //       "left-0",
+  //       "gap-10",
+  //     ];
+  //   } else {
+  //     menuClasses = ["hidden", "md:flex"];
+  //   }
+
+  //   return menuClasses.join(" ");
+  // }
 
   return (
     <header>
-      <nav className="flex items-center justify-between">
+      <nav className="z-10 flex items-center justify-between flex-col md:flex-row">
         <Link href="/">
           <h1 className="text-2xl font-semibold hover:text-sandrift active:text-sandrift transition">
             evanmarshall<span className="text-sunset_orange">.</span>
             <span className="text-sandrift">dev</span>
           </h1>
         </Link>
-        <div className="flex items-center gap-5 text-lg text-gray-300">
+        <div
+          className={`${isOpen ? "flex" : "hidden"} flex-col md:flex md:flex-row md:items-center md:gap-5 md:text-lg md:text-gray-300`}
+        >
+          {/* <div className={getMenuClasses()}> */}
           {pages.map((page) => {
             return (
               <Link
                 key={page._id}
                 href={`/${page.slug}`}
                 className={clsx(
-                  "hover:text-sandrift hover:underline active:text-sandrift active:underline transition px-3 py-2",
+                  "hover:underline active:underline transition px-3 py-2",
                   {
-                    "text-sandrift bg-wild_sand rounded":
+                    "text-ebony bg-wild_sand rounded":
                       pathname === `/${page.slug}`,
                   }
                 )}
@@ -57,6 +84,38 @@ export default function Navbar({ pages }: NavbarProps) {
           >
             Email Me
           </a>
+        </div>
+        {/* Mobile menu button. */}
+        <div className="md:hidden flex items-center">
+          <button
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              )}
+            </svg>
+          </button>
         </div>
       </nav>
     </header>
